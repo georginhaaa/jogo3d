898f5bb4a1b2fa3447d6dee6e04fc12fbbf311cc
Georgia Paulina Pereira Bezerra <20211170150026@ifrn.local>
2023-10-06 10:01:36 -0300

    jogo ate parte 9

---
+++ My project/Assets/scripts/Player.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Player : MonoBehaviour
+{
+
+    private CharacterController controller;
+    public float speed;
+    public float smoothRotTime;
+    private float turnSmoothVelocity;
+    private Transform cam;
+    
+    Vector3 moveDirection;
+    public float gravity;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        cam = Camera.main.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        if (controller.isGrounded)
+        {
+            float horizontal = Input.GetAxisRaw("Horizontal");
+            float vertical = Input.GetAxisRaw("Vertical");
+
+            Vector3 direction = new Vector3(horizontal, 0f, vertical);
+
+            if (direction.magnitude > 0)
+            {
+                float angle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+                float smoothAngle =
+                    Mathf.SmoothDampAngle(transform.eulerAngles.y, angle, ref turnSmoothVelocity, smoothRotTime);
+
+                transform.rotation = Quaternion.Euler(0f, smoothAngle, 0f); 
+                moveDirection = Quaternion.Euler(0f, angle, 0f) * Vector3.forward * speed;
+                
+
+            }
+            else
+            {
+                moveDirection = Vector3.zero;
+            }
+        }
+
+        moveDirection.y -= gravity * Time.deltaTime;
+        
+        controller.Move(moveDirection * speed * Time.deltaTime);
+        
+        
+    }
+}
+       
+        
+    
